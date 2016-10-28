c16224d18d4f856a3b02ab772affb00c83ac90ca
Armagedescu <armagedescu@gmail.com>
2016-10-28 09:16:42 +0300

    bezier

--- trunk/anul1/semestrul1/Programarea3D/lab1/bezier.cpp
+++ trunk/anul1/semestrul1/Programarea3D/lab1/bezier.cpp
@@ -3,19 +3,12 @@
 #include <GL/glu.h>
 #include <stdlib.h>
 #include <GL/glut.h>
-extern void spacePrepare();
-extern void drawCentralPoint();
-extern void drawAxis();
 static GLfloat ctrlpoints[4][3] = {
         { -4.0, -4.0, 0.0}, { -2.0, 4.0, 0.0}, 
-        {2.0, -4.0, 0.0}, {4.0, 4.0, 0.0}};
+        {  2.0, -4.0, 0.0}, {  4.0, 4.0, 0.0}};
 
 static void init(void)
 {
-	spacePrepare();
-	
-
-
    glClearColor(0.0, 0.0, 0.0, 0.0);
    glShadeModel(GL_FLAT);
    glMap1f(GL_MAP1_VERTEX_3, 0.0, 1.0, 3, 4, &ctrlpoints[0][0]);
@@ -25,21 +18,19 @@ static void init(void)
 static void display(void)
 {
    int i;
-	drawCentralPoint();
-	drawAxis();
    glClear(GL_COLOR_BUFFER_BIT);
    glColor3f(1.0, 0.0, 0.0);
    glBegin(GL_LINE_STRIP);
       for (i = 0; i <= 30; i++) 
-         glEvalCoord1f((GLfloat) i/30.0);
+		glEvalCoord1f((GLfloat) i/30.0);
    glEnd();
    /* The following code displays the control points as dots. */
-   //glPointSize(5.0);
-   //glColor3f(1.0, 1.0, 0.0);
-   //glBegin(GL_POINTS);
-   //   for (i = 0; i < 4; i++) 
-   //      glVertex3fv(&ctrlpoints[i][0]);
-   //glEnd();
+   glPointSize(5.0);
+   glColor3f(1.0, 1.0, 0.0);
+   glBegin(GL_POINTS);
+      for (i = 0; i < 4; i++) 
+         glVertex3fv(&ctrlpoints[i][0]);
+   glEnd();
    glFlush();
 }
 
@@ -60,12 +51,13 @@ static void reshape(int w, int h)
 
 
 
-int bezierMain(int argc, char** argv)
+int bezierGlutMain(int argc, char** argv)
 {
    glutInit(&argc, argv);
 
 
    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+
    glutInitWindowSize (500, 500);
    glutInitWindowPosition (100, 100);
    glutCreateWindow (argv[0]);
@@ -73,5 +65,19 @@ int bezierMain(int argc, char** argv)
    glutDisplayFunc(display);
    glutReshapeFunc(reshape);
    glutMainLoop();
+   return 0;
+}
+#include <gl/GLAux.h>
+void CALLBACK auxBezierDisplay(void){display();}
+void CALLBACK auxBezierReshape(GLsizei x, GLsizei y){reshape(x, y);}
+int bezierAuxMain(int argc, char** argv)
+{
+   auxInitDisplayMode( AUX_RGB | AUX_SINGLE);
+   auxInitPosition( 100, 100, 500, 500);
+   auxInitWindow((LPCWSTR)("OpenGL"));
+   auxIdleFunc(auxBezierDisplay);
+   auxReshapeFunc(auxBezierReshape);
+   init ();
+   auxMainLoop(auxBezierDisplay);
    return 0;
 }
--- trunk/anul1/semestrul1/Programarea3D/lab1/main.cpp
+++ trunk/anul1/semestrul1/Programarea3D/lab1/main.cpp
@@ -7,12 +7,15 @@
 
 #include "externals.h"
 
-int bezierMain(int argc, char** argv);
+int bezierGlutMain(int argc, char** argv);
+int bezierAuxMain(int argc, char** argv);
 int glutMain(int argc, char** argv);
 int auxMain(int argc, char** argv);
 
 int main(int argc, char** argv)
 {
+	return bezierAuxMain( argc,  argv);
+	return bezierGlutMain( argc,  argv);
 	//return auxMain(argc, argv);
 	return glutMain(argc, argv);
 }
