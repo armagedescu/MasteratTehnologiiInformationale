d9c71cfde013f36e6eca2b09906762f108401bef
Armagedescu <armagedescu@gmail.com>
2017-02-28 00:39:54 +0200

    Prelucrarea Semnalelor Digitale

---
+++ PrelucrareaSemnalelorDigitale/PrelucrareaSemnalelorDigitale/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace PrelucrareaSemnalelorDigitale
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Console.WriteLine("salut");
+            Application.Run(new Form1());
+        }
+    }
+}
